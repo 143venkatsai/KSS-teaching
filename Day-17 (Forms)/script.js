46961855a499bd8cd3d50fe03a95fd3e4193859c
143venkatsai <venkatsaipelluru143@gmail.com>
2025-01-30 20:17:39 +0530

    day-17 working withs forms

---
+++ Day-17 (Forms)/script.js
@@ -0,0 +1,29 @@
+let formElement = document.getElementById("formElement");
+
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+
+let emailError = document.getElementById("emailError");
+let passwordError = document.getElementById("passwordError");
+
+email.addEventListener("blur", function (e) {
+  console.log(e.target.value);
+  if (e.target.value === "") {
+    emailError.textContent = "Required*";
+  } else {
+    emailError.textContent = "";
+  }
+});
+
+password.addEventListener("blur", function (e) {
+  console.log(e.target.value);
+  if (e.target.value === "") {
+    passwordError.textContent = "Required*";
+  } else {
+    passwordError.textContent = "";
+  }
+});
+
+formElement.addEventListener("submit", function (e) {
+  e.preventDefault();
+});
